a978661891322d86a113b8e06a7d19553b20c0cc
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2025-01-07 09:29:31 +0330

    chore: update About section to enhance personal introduction and highlight key skills

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -17,14 +17,17 @@ const About = () => {
       <div className="container">
         <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
           <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
-            I&#39;m Mohammad Reza Ghasemi, a frontend developer with over four
-            years of experience building dynamic, user-friendly web
-            applications. Passionate about creating seamless digital
-            experiences, I specialize in React, Next.js, TypeScript, and Node.js
-            to craft scalable and responsive designs. I&#39;ve had the
-            opportunity to work with companies like Snapp, SADAD and Fanap,
-            contributing to projects that emphasize functionality and design
-            excellence. Let&#39;s connect and build something amazing together!
+            I&#39;m Mohammad Reza Ghasemi, a Frontend Developer with over four
+            years of experience building and maintaining web applications using
+            React, Next.js, and TypeScript. I specialize in creating scalable UI
+            components, optimizing performance, and collaborating with teams to
+            deliver user-friendly solutions. Throughout my career, I&#39;ve had
+            the opportunity to work with leading companies such as SnappPay,
+            Fanap Plus, and Sadad, where I developed dynamic features like
+            recursive comment systems, custom icon packages, and improved data
+            fetching strategies. My passion for clean code and efficiency drives
+            me to deliver high-quality, user-centric web experiences that meet
+            business goals.
           </p>
           <div className="flex flex-wrap items-center gap-4 md:gap-7">
             {aboutItems.map(({ number, label }, key) => (
